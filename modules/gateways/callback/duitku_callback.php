ac972f2ed1ecddcfd067d4195dfc5fc04d84e132
KangLeian <rayhan@duitku.com>
2025-04-16 17:32:21 +0700

    1.3.0

---
+++ modules/gateways/callback/duitku_callback.php
@@ -0,0 +1,112 @@
+<?php
+require "../../../init.php";
+$whmcs->load_function("gateway");
+$whmcs->load_function("invoice");
+
+require_once __DIR__ . '/../duitku-lib/Duitku.php';
+
+$gatewayModuleName = "";
+
+// check if the module is activated
+/*--- start ---*/
+$paymentCode= stripslashes($_POST['paymentCode']);
+
+switch ($paymentCode) {
+	case "OV":
+		$gatewayModuleName = "duitku_ovo"; break;
+	case "VC":
+		$gatewayModuleName = "duitku_cc"; break;
+	case "BK":
+		$gatewayModuleName = "duitku_bca"; break;
+	case "BT":
+		$gatewayModuleName = "duitku_vapermata"; break;
+	case "VA":
+		$gatewayModuleName = "duitku_vamaybank"; break;
+	case "A1":
+		$gatewayModuleName = "duitku_vaatmbersama"; break;
+	case "B1":
+		$gatewayModuleName = "duitku_vacimb"; break;
+	case "I1":
+		$gatewayModuleName = "duitku_vabni"; break;
+	case "M1":
+		$gatewayModuleName = "duitku_vamandiri"; break;
+	case "FT":
+		$gatewayModuleName = "duitku_varitel"; break;
+    default:
+		throw new Exception('payment method not recognize.');
+}
+
+$gatewayParams = getGatewayVariables($gatewayModuleName);
+if (!$gatewayParams['type']) {
+	exit("Module Not Activated");
+}
+
+/*--- end ---*/
+
+if (empty($_POST['resultCode']) || empty($_POST['merchantOrderId']) || empty($_POST['reference'])) {
+	throw new Exception('wrong query string please contact admin.');
+}    
+
+$order_id = stripslashes($_POST['merchantOrderId']);
+$status = stripslashes($_POST['resultCode']);
+$reference = stripslashes($_POST['reference']);
+$paymentAmount = stripslashes($_POST['amount']);
+//set parameters for Duitku inquiry
+$merchant_code = $gatewayParams['merchantcode'];
+$api_key = $gatewayParams['serverkey'];
+$endpoint = $gatewayParams['endpoint'];
+
+/**
+ * Validate Callback Invoice ID.
+ *
+ * Checks invoice ID is a valid invoice number. Note it will count an
+ * invoice in any status as valid.
+ *
+ * Performs a die upon encountering an invalid Invoice ID.
+ *
+ * Returns a normalised invoice ID.
+ */
+
+$order_id = checkCbInvoiceID($order_id, $gatewayParams['name']);
+checkCbTransID($reference);
+$success = false;
+     
+if ($status == '00' && Duitku_VtWeb::validateTransaction($endpoint, $merchant_code, $order_id, $reference, $api_key)) {
+	$success = true;
+} else {
+	$success = false;
+}     
+
+if ($success) {
+    /**
+     * Add Invoice Payment.
+     *
+     * Applies a payment transaction entry to the given invoice ID.
+     *
+     * @param int $invoiceId         Invoice ID
+     * @param string $transactionId  Transaction ID
+     * @param float $paymentAmount   Amount paid (defaults to full balance)
+     * @param float $paymentFee      Payment fee (optional)
+     * @param string $gatewayModule  Gateway module name
+     */
+    addInvoicePayment(
+        $order_id,
+        $reference,
+        $paymentAmount,
+        0,
+        $gatewayModuleName
+    );    
+    echo "Payment success notification accepted";
+}
+else{
+	//Adopted from paypal to log all the failed transaction
+	$orgipn = "";
+	foreach ($_POST as $key => $value) {
+		$orgipn.= ("" . $key . " => " . $value . "\r\n");		
+	}
+	logTransaction($gatewayModuleName, $orgipn, "Duitku Handshake Invalid");
+	header("HTTP/1.0 406 Not Acceptable");
+	exit();
+}
+
+?>
---
+++ modules/gateways/duitku_vamandiri.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * WHMCS Duitku Payment Gateway Module
+ *
+ * Duitku Payment Gateway modules allow you to integrate Duitku Web with the
+ * WHMCS platform.
+ *
+ * For more information, please refer to the online documentation.
+ * @see http://docs.duitku.co.id
+ *
+ * Module developed based on official WHMCS Sample Payment Gateway Module
+ * 
+ * @author timur@chakratechnology.com
+ */
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+require_once(dirname(__FILE__) . '/duitku-lib/Duitku.php');
+/**
+ * Define module related meta data.
+ *
+ * Values returned here are used to determine module related capabilities and
+ * settings.
+ *
+ * @see http://docs.whmcs.com/Gateway_Module_Meta_Data_Parameters
+ *
+ * @return array
+ */
+function duitku_vamandiri_MetaData()
+{
+    return array(
+        'DisplayName' => 'Duitku VA MANDIRI Payment Gateway Module',
+        'APIVersion' => '1.0', // Use API Version 1.1
+        'DisableLocalCredtCardInput' => true,
+        'TokenisedStorage' => true,
+    );
+}
+/**
+ * Define gateway configuration options.
+ *
+ * The fields you define here determine the configuration options that are
+ * presented to administrator users when activating and configuring your
+ * payment gateway module for use.
+ *
+ * Supported field types include:
+ * * text
+ * * password
+ * * yesno
+ * * dropdown
+ * * radio
+ * * textarea
+ *
+ * Examples of each field type and their possible configuration parameters are
+ * provided in the sample function below.
+ *
+ * @return array
+ */
+function duitku_vamandiri_config()
+{
+    return array(
+        // the friendly display name for a payment gateway should be
+        // defined here for backwards compatibility
+        'FriendlyName' => array(
+            'Type' => 'System',
+            'Value' => 'Duitku VA MANDIRI',
+        ),
+        // a text field type allows for single line text input
+        'merchantcode' => array(
+            'FriendlyName' => 'Duitku Merchant Code',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Input Duitku Merchant Code.',
+        ),
+        // a text field type allows for single line text input
+        'serverkey' => array(
+            'FriendlyName' => 'Duitku API Key',
+            'Type' => 'text',
+            'Size' => '50',
+            'Default' => '',
+            'Description' => 'Input Duitku API Key.',
+        ),
+        // the dropdown field type renders a select menu of options
+        'endpoint' => array(
+            'FriendlyName' => 'Duitku Endpoint',
+            'Type' => 'text',
+			'Size' => '100',
+			'Default' => 'https://passport.duitku.com/webapi',
+            'Description' => 'Duitku Endpoint, mohon isi merchant code dan api key sebelum mengakses endpoint.',
+        ),        
+    );
+}
+/**
+ * Payment link.
+ *
+ * Required by third party payment gateway modules only.
+ *
+ * Defines the HTML output displayed on an invoice. Typically consists of an
+ * HTML form that will take the user to the payment gateway endpoint.
+ *
+ * @param array $params Payment Gateway Module Parameters
+ *
+ * @see http://docs.whmcs.com/Payment_Gateway_Module_Parameters
+ *
+ * @return string
+ */
+function duitku_vamandiri_link($params)
+{
+	//set parameters for Duitku inquiry
+    $merchant_code = $params['merchantcode'];
+    $amount = (int)$params['amount'];
+	$order_id = $params['invoiceid'];	
+	$serverkey = $params['serverkey'];
+	$endpoint = $params['endpoint'];
+	
+	
+	//System parameters
+	$companyName = $params['companyname'];
+	$systemUrl = $params['systemurl'];
+    $returnUrl = $params['returnurl'];
+	$langPayNow = $params['langpaynow'];
+	
+	// Client Parameters
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname = $params['clientdetails']['lastname'];
+    $email = $params['clientdetails']['email'];
+	$phoneNumber = $params['clientdetails']['phonenumber'];
+	
+	$signature = md5($merchant_code.$order_id.$amount.$serverkey);
+	
+	// Prepare Parameters	
+    $params = array(
+          'merchantCode' => $merchant_code, // API Key Merchant /
+          'paymentAmount' => $amount, //transform order into integer
+          'paymentMethod' => "M1",
+          'merchantOrderId' => $order_id,
+          'productDetails' => $companyName . ' Order : #' . $order_id,
+          'additionalParam' => '',
+          'merchantUserInfo' => $firstname . " " .  $lastname,
+		  'email' => $email,
+		  'phoneNumber' => $phoneNumber,
+          'signature' => $signature,          
+          'returnUrl' => $systemUrl."/modules/gateways/callback/duitku_return.php",
+          'callbackUrl' => $systemUrl."/modules/gateways/callback/duitku_callback.php"
+    );         
+
+    try {     
+      $redirUrl = Duitku_VtWeb::getRedirectionUrl($endpoint, $params);      
+    }
+    catch (Exception $e) {
+      error_log('Caught exception: '.$e->getMessage()."\n");
+    }		
+	
+	$img       = $systemUrl . "/modules/gateways/duitku-images/duitku_vamandiri.png"; 
+    $htmlOutput .= '<img style="width: 152px;" src="' . $img . '" alt="VA MANDIRI"><br>';
+	$htmlOutput .= '<button onClick="javascript:window.location.href=\'' . $redirUrl . '\'">' . $langPayNow . '</button>';
+    
+    
+    return $htmlOutput;
+}
